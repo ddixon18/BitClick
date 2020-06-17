092ea4a9502d2630e27c331a9786895e2829ee14
ddixon18 <43619214+ddixon18@users.noreply.github.com>
2020-06-17 14:38:13 -0400

    Style Update

--- Start.java
+++ Start.java
@@ -1,10 +1,8 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JLabel;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -13,7 +11,7 @@ import javax.swing.SwingConstants;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class Menu {
+public class Start {
 
 	public JFrame frame;
 	public static String[] thing;
